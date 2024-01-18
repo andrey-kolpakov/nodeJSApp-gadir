dca253ecbb98ee40266dd834c969fefe03a1aa06
Andrey Kolpakov <gfstels@yandex.ru>
2024-01-18 17:51:22 +0600

    new

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
-// Установите необходимые зависимости перед использованием:
-// npm install node-cron axios
+// // Установите необходимые зависимости перед использованием:
+// // npm install node-cron axios
 
-const cron = require('node-cron');
-const axios = require('axios');
+// const cron = require('node-cron');
+// const axios = require('axios');
 
-// Замените URL и параметры на свои
-const chatUrl = 'https://interface.gadir-jeweler.kz/';
-const chatMessage = 'Проверка';
+// // Замените URL и параметры на свои
+// const chatUrl = 'https://interface.gadir-jeweler.kz/';
+// const chatMessage = 'Проверка';
 
-// Запускаем cron job каждые 1 минут
-cron.schedule('*/1 * * * *', () => {
-  // Отправляем сообщение в чат
-  axios.post(chatUrl, { message: chatMessage })
-    .then(response => {
-      console.log('Сообщение успешно отправлено:', response.data);
-    })
-    .catch(error => {
-      console.error('Ошибка при отправке сообщения:', error.message);
-    });
-});
+// // Запускаем cron job каждые 1 минут
+// cron.schedule('*/1 * * * *', () => {
+//   // Отправляем сообщение в чат
+//   axios.post(chatUrl, { message: chatMessage })
+//     .then(response => {
+//       console.log('Сообщение успешно отправлено:', response.data);
+//     })
+//     .catch(error => {
+//       console.error('Ошибка при отправке сообщения:', error.message);
+//     });
+// });
 
-console.log('Планировщик задач запущен');
+// console.log('Планировщик задач запущен');
+
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res){
+
+    res.set('Content-Type', 'text/html; charset=utf-8')
+    res.send("<h1>Hello</h1>")
+    
+})
+
+app.listen(process.env.PORT || 3000)
