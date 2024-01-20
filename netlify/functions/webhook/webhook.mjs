e6b1debbdf547b1295c902e332de390a3f6f1d9e
Andrey Kolpakov <gfstels@yandex.ru>
2024-01-20 19:13:22 +0600

    new

--- netlify/functions/webhook/webhook.mjs
+++ netlify/functions/webhook/webhook.mjs
@@ -93,7 +93,7 @@ const handler = async (event, context) => {
         //     console.log('123123')
         //     console.log(response.data)
         // })
-
+        console.log('123')
         const bot = new TelegramBot(botToken, { polling: false })
         console.log(bot)
         bot.sendMessage(chatID, textMessageForGadir)
