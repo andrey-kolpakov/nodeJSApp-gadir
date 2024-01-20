b01ce20348e42f980597e9ac59da45278fa79387
Andrey Kolpakov <gfstels@yandex.ru>
2024-01-20 11:18:40 +0600

    new

--- netlify/functions/webhook/webhook.js
+++ netlify/functions/webhook/webhook.js
@@ -1,10 +1,28 @@
 exports.handler = async (event, context) => {
-  try {
-    console.log(event.body)
-    return {
-      statusCode: 200
+    try {
+        console.log(event.body)
+        const webhookString = event.body
+
+        // Декодирование строки
+        const decodedString = decodeURIComponent(webhookString)
+
+        // Разделение строки на массив пар ключ-значение
+        const keyValuePairs = decodedString.split('&')
+
+        // Создание объекта
+        const webhookObject = {}
+        keyValuePairs.forEach((pair) => {
+            const [key, value] = pair.split('=')
+            webhookObject[key] = value
+        })
+
+        // Вывод объекта в консоль
+        console.log(webhookObject)
+
+        return {
+            statusCode: 200,
+        }
+    } catch (err) {
+        return { statusCode: 500, body: err.toString() }
     }
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() }
-  }
 }
