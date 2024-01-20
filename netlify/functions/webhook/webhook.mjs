23d246474eed34234449455319d21a1515892eda
Andrey Kolpakov <gfstels@yandex.ru>
2024-01-20 18:37:48 +0600

    new

--- netlify/functions/webhook/webhook.mjs
+++ netlify/functions/webhook/webhook.mjs
@@ -94,9 +94,9 @@ const handler = async (event, context) => {
 
         const chatID = '-1001992000184'
         const botToken = '6391665621:AAFI8eS-466kwW1142OdQKthJ-_hLHOLyxM'
-        const botUrl = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatID}&parse_mode=html&text=12312312312`
+        const botUrl = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatID}&parse_mode=html&text=${textMessageForGadir}`
 
-        await axios.post(botUrl, newObj.name).then((response) => {
+        await axios.post(botUrl, '123').then((response) => {
             console.log('123123')
             console.log(response.data)
         })
