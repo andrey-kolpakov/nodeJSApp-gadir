1393b249330e2f81602c0a456df0f3a2d99e7833
Andrey Kolpakov <gfstels@yandex.ru>
2024-01-21 13:44:48 +0600

    new

--- netlify/functions/webhook/webhook.mjs
+++ netlify/functions/webhook/webhook.mjs
@@ -1,11 +1,12 @@
 import axios from 'axios'
 
 import { Client } from 'amocrm-js'
-import { Token } from '../../../Token.mjs'
+import { Token } from '/tmp/Token.mjs/'
 
 import TelegramBot from 'node-telegram-bot-api'
 
 import { writeFile } from 'fs/promises'
+
 
 const handler = async (event, context) => {
     try {
@@ -42,15 +43,6 @@ const handler = async (event, context) => {
     async function run(info) {
         console.log('run run run')
 
-        try {
-            const filePath = '/tmp/Token123.mjs'
-            await writeFile(filePath, `export const Token = ${JSON.stringify('newToken', null, 2)};`)
-
-            console.log('Файл успешно перезаписан.')
-        } catch (error) {
-            console.error('Произошла ошибка при перезаписи файла:', error)
-        }
-
         const client = new Client({
             // логин пользователя в портале, где адрес портала domain.amocrm.ru
             domain: 'liyavais1408.amocrm.ru', // может быть указан полный домен вида domain.amocrm.ru, domain.amocrm.com
@@ -62,7 +54,7 @@ const handler = async (event, context) => {
                 client_id: 'fc34dd9d-ae87-4650-8a02-43272565abbf', // ID интеграции
                 client_secret: 'Dmzl1bL4QP3my1vxFXLh96ivonlumrPqxbfA8kYa9gfrU4FGN6ax66dP9xCaWnh7', // Секретный ключ
                 redirect_uri: 'https://ya.ru/', // Ссылка для перенаправления
-                code: 'def502009fe76c50a27dd5482f330f67438634c0244059bb3ff2d2957a3a8a3e62b4c2a130dc4464471b3d698623eab1a452032f6685b0ab7f25e0452d9edea81a2cf56473a0dd46a597edea7063ad8a067bc739db49567b3d39bbc01c9d676565306ba72b316b2f0f3b492884455481412b8ba1aad0552e8887a5b96f52e78d624dc6ce03bf3f1a67c222f66e2f05f3a388d25f8ddda177d01b0d3a78d4be59d3e5bda0683163e2eb5e79fa866847cf14e5e1180ffc05826160091d350682669c3b1044bf1a5d8913399ef2599af2f2b1e483ecb2f797ff5812616ee0ca0ab610432ba75c7468fe5aa69ac73ddb1d07fcdd2d7928d7ad2e49e7372ccba76e2af7ce771f5b802a40dcea7c3e9be5bc6085dfb8c8d367ce079c8ac1147e432ecd7e83ba0e75ab04bcf9b0e1a7358180fb80003e37a17a52868111894481e6ef3276c404773c55ab0a90ba0788b703d491d83674d943f775439af972e48e7ebefbb288cd219b5d86d977ed7593c0c8e29edee9da5424bfe07daf1a3ef0c22d0326c9de7f71e2602225c2913b84f65a5237102363dd4807edb6cdc8c4b33bad07162dd137506ac3c5052548af9eee3b4c34454abab4f36accb2324468aa41b821c98ba16b04b563185c00575303ed85b0ab711717a29b159e49c2207948eb7c5536', // Код для упрощённой авторизации, необязательный
+                code: 'def5020071eff8de5eabe891c19e081d346e0aba0ffea56e5ac05e479fccc9c43596e37dba48560528c0a623dbd163d46fe06a6f611844ec97df30a991ff058a6c8af6253153d47b6384b96f390d9863c552cd2e208d00b59e44f560f00dfddb36fbb1bf03b0ad74ab5a2158b172a3e1ad63bbd8b96f523bb2da9def969ff618dd799974cae511b61e18d24897c72094f84c401b37c2feeb39d30c6343cc80a1f20701cf98d6234af633fe9307f9ab3d4ff9dab0521fe87b64f282865520457c06277f6364912d228c8dd15c3f018e6dbdf6fa387936ef83bf9d1edbcfbd85299b349ff8253249e573bb34f3a8e3c574ce331ccc951716a4ba21ffdfdd7f9ac624ba487de51bf66597a220252f77d5f6e6a546ab1451ce40d71ee6f8f224253160ee48aae75b73910d01ca9e08573f085c4cd8c090abf265347b637080e18eb87bb2d701b2646e8d7766d074096c352148d85a6e28aeb62ad31090eb01a38a4679f038cccf0c2d9bb49cd19cad1a9653f1944c2612db53734d14f9980a1f22c1c3fe6d87f6c17c9c27dc1aec76920229cee4a3b72ffda026cf8920210deb70d10f5793368f017194ce749f3f9776982ba0db787515692783c1895fa94e5c5238bc35d9cc8a5cc8ec27a2005b88cff486ae99f08f00b4a7fac51abf7615eb5527', // Код для упрощённой авторизации, необязательный
             },
         })
 
@@ -71,7 +63,14 @@ const handler = async (event, context) => {
         const auth = await client.connection.connect()
         const newToken = client.token.getValue()
 
-        
+        try {
+            const filePath = '/tmp/Token.mjs'
+            await writeFile(filePath, `export const Token = ${JSON.stringify(newToken, null, 2)};`)
+
+            console.log('Файл успешно перезаписан.')
+        } catch (error) {
+            console.error('Произошла ошибка при перезаписи файла:', error)
+        }
 
         const lead = await client.leads.getById(info['leads[status][0][id]'], { with: ['contacts'] })
 
